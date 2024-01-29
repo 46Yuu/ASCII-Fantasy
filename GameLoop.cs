dcc1ea7baebeedc62a2f3529ee3bc45c0a30169f
Draknut <60214040+Draknut@users.noreply.github.com>
2024-01-29 16:34:33 +0100

    GameLoop Test

--- GameLoop.cs
+++ GameLoop.cs
@@ -1,19 +1,28 @@
-﻿
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
 
-
-void Main()
+namespace ASCIIFantasy
 {
-    private bool isRunning = true;
-    while (isRunning)
+    class GameLoop
     {
-        // Inputs
+        //private Gam _myGame;
+        private bool isRunning = true;
+        DateTime _previousGameTime = DateTime.Now;
+        private async void Loop(){
+            while(isRunning)
+            {
+                // Calculate the time elapsed since the last game loop cycle
+                TimeSpan GameTime = DateTime.Now - _previousGameTime;
+                // Update the current previous game time
+                _previousGameTime = _previousGameTime + GameTime;
+                // Update the game
+                    ///_myGame.Update(GameTime);
+                // Update Game at 60fps
+                await Task.Delay(8);
 
-        // Update
-
-        // Draw
-
-
-        Console.WriteLine("Oui");
+            }
+        }
     }
 }
 
