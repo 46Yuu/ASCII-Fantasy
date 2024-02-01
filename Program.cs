da90f1782ad242e1ae52a18874a37807fc889e36
Jehyann <camille.gorsse@gmail.com>
2024-02-01 09:51:03 +0100

    done

--- Program.cs
+++ Program.cs
@@ -4,18 +4,60 @@ class Program
 {
     static void Main()
     {
+        Console.CursorVisible = false;
 
-        Map map = new Map(120, 28); // Taille de la carte en colomnes et lignes
+        Map map = new Map(120, 28);
+        Menu mainMenu = new Menu(new string[] { "NEW GAME", "LOAD GAME", "EXIT" });
 
         while (true)
         {
-            //map.DrawHouse(10, 10);
+            mainMenu.Display();
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            Console.Clear();
+
+            if (keyInfo.Key == ConsoleKey.Enter)
+            {
+                string selectedOption = mainMenu.GetSelectedOption();
+
+                if (selectedOption == "NEW GAME")
+                {
+                    RunGame(map);
+                }
+                else if (selectedOption == "LOAD GAME")
+                {
+                    // Faudra implémenter le chargement de partie ici
+                }
+                else if (selectedOption == "EXIT")
+                {
+                    Console.WriteLine("Thank you for playing !");
+                    Environment.Exit(0);
+                }
+            }
+            else
+            {
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        mainMenu.MoveUp();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        mainMenu.MoveDown();
+                        break;
+                }
+            }
+        }
+    }
+
+    static void RunGame(Map map)
+    {
+        while (true)
+        {
             map.DisplayMap();
 
             ConsoleKeyInfo keyInfo = Console.ReadKey();
             Console.Clear();
 
-            // Déplacement
             switch (keyInfo.Key)
             {
                 case ConsoleKey.UpArrow:
@@ -29,9 +71,6 @@ class Program
                     break;
                 case ConsoleKey.RightArrow:
                     map.MovePlayer(1, 0);
-                    break;
-                default:
-                    Console.WriteLine("Unrecognized key. Use the arrows to move.");
                     break;
             }
         }
